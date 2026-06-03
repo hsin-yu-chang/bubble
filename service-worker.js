bd422fde5c90aec5389d1bbd5659c4f2e9acf92b
hsin-yu-chang <73873533+hsin-yu-chang@users.noreply.github.com>
2026-06-03 16:12:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,37 @@
-const CACHE_NAME = "bubble-translation-v1";
+const CACHE_NAME = "bubble-translation-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
   "./favicon.ico",
-  "./vite.svg",
-  "./cloudinary_urls.txt",
-
-  "./assets/index-brVkwgov.js",
-  "./assets/index-d5z48If.css",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./assets/main.js",
+  "./assets/style.css",
   "./assets/translation_patch.js"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
